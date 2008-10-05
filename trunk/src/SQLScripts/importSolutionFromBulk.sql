d31e9370af32695496c40770aef73ce48030f21e
fpadurou <fpadurou@f299efa3-7055-0410-877e-572f964951e2>
2008-10-05 20:04:09 +0000

    make sol-part link

--- trunk/src/SQLScripts/importSolutionFromBulk.sql
+++ trunk/src/SQLScripts/importSolutionFromBulk.sql
@@ -18,7 +18,7 @@ implCost, implCostEur, sapDiscount, dbUsed, SAPBusUsed, SAPGUIUsed, compA1B1Used
 otherIT, addRemarks, solSAPMicroSite, lastPartRevieDate, reviewedBy, profileAdded, dateCreated, modifiedBy,
 dateUpdated, notificationProc, notificationText, companyId, maturityId, statusByProvider, statusBySAP, solUserType, countryPriceEuro, solMaturity
 ) select record_id, 
-SUBSTRING(_1,LOCATE('[',_1)+1, LOCATE(']',_1) - LOCATE('[',_1)-1),
+SUBSTRING(_1,LOCATE('[',_1)+1, LOCATE(']',_1) - LOCATE('[',_1)-1), 
 _2, sol_name, sol_desc, site,
 str_to_date( date1,  '%d-%b-%Y %T' ),
 str_to_date( date2,  '%d-%b-%Y %T' ),
@@ -28,7 +28,7 @@ _34, CAST(app_price AS UNSIGNED),
 CAST(_37 AS UNSIGNED), CAST(_38 AS UNSIGNED), CAST(_39 AS UNSIGNED), CAST(_40 AS UNSIGNED), CAST(_41 AS UNSIGNED), CAST(_42 AS UNSIGNED), CAST(_43 AS UNSIGNED),
 CAST(_44 AS UNSIGNED), CAST(_45 AS UNSIGNED), _47, _50, _51, _52, _53, _55, _56,
 _57, _58, sol_site_MS, str_to_date( LastRevDate,  '%d-%b-%Y %T' ), _61, _62, str_to_date( Date_created, '%m/%d/%Y %H:%i'),_64,
-str_to_date( Date_updated, '%m/%d/%Y %H:%i'), _66, _67, CAST(_38 AS UNSIGNED), CAST(_38 AS UNSIGNED), CAST(_38 AS UNSIGNED), CAST(_38 AS UNSIGNED), CAST(_38 AS UNSIGNED), CAST(_38 AS UNSIGNED), CAST(_38 AS UNSIGNED)
+str_to_date( Date_updated, '%m/%d/%Y %H:%i'), _66, _67, CAST(SUBSTRING(_1,LOCATE('[',_1)+1, LOCATE(']',_1) - LOCATE('[',_1)-1) AS UNSIGNED),  CAST(_38 AS UNSIGNED), CAST(_38 AS UNSIGNED), CAST(_38 AS UNSIGNED), CAST(_38 AS UNSIGNED), CAST(_38 AS UNSIGNED), CAST(_38 AS UNSIGNED)
 from soldirectorytaball;
 
 SELECT date_format( sapCertSince , '%m/%d/%Y' ) AS my_date1 FROM sol_directory_tmp ;
